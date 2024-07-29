d868aeb337759a0a61e39a424a1e840b8fcec4fa
Patrick Adams <patrick_aust@hotmail.com>
2024-07-29 13:25:24 +1000

    a

--- setup-recon.py
+++ setup-recon.py
@@ -3,6 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+data_dir = '/home/ec2-user/dev/data/
+
+
+
+
+
+
+nq = 300
+nl = 250
+npsi = 360*32
+
+qmax = 3.5
+qmin = 0.0725
+nchunks = 32
 
 
 
